6029f2237aea6da75a8cce8cf3ed1b1639f04cc5
Muhammad Arief Huda <ariefhuda434@gmail.com>
2026-06-14 17:06:28 +0700

    adjust promo

--- src/data/ticketPackages.js
+++ src/data/ticketPackages.js
@@ -1,9 +1,9 @@
 export const ticketPackages = [
-    { id: 'single', name: 'Single', qty: 1, price: 20000, note: '1 tiket masuk' },
-    { id: 'duo', name: 'Duo', qty: 2, price: 40000, note: '2 tiket masuk' },
-    { id: 'trio', name: 'Trio', qty: 3, price: 55000, note: 'Hemat untuk bertiga', popular: true },
-    { id: 'squad', name: 'Squad', qty: 4, price: 75000, note: 'Paket komunitas kecil' },
-    { id: 'mates', name: 'Mates', qty: 6, price: 110000, note: '6 tiket masuk' },
-    { id: 'party', name: 'Party', qty: 8, price: 150000, note: '8 tiket masuk' },
-    { id: 'bersapma', name: 'Bersapma', qty: 15, price: 280000, note: '14 tiket + free 1' }
+    { id: 'single', name: 'Single', qty: 1, price: 20000, note: '1 tiket masuk + Voucher Timezone' },
+    { id: 'duo', name: 'Duo', qty: 2, price: 40000, note: '2 tiket masuk + Voucher Timezone' },
+    { id: 'trio', name: 'Trio', qty: 3, price: 55000, note: 'Hemat untuk bertiga + Voucher Timezone', popular: true },
+    { id: 'squad', name: 'Squad', qty: 4, price: 75000, note: 'Paket komunitas kecil + Voucher Timezone' },
+    { id: 'mates', name: 'Mates', qty: 6, price: 110000, note: '6 tiket masuk + Voucher Timezone' },
+    { id: 'party', name: 'Party', qty: 8, price: 150000, note: '8 tiket masuk + Voucher Timezone' },
+    { id: 'bersapma', name: 'Bersapma', qty: 15, price: 280000, note: '14 tiket + free 1 + Voucher Timezone' }
   ]
